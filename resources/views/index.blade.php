604bc982b1573cacc51c40ec636970c5ec301ad6
MinotourDmitriy <Kkunkka1@mail.ru>
2023-04-12 01:23:54 +0300

    Добавил блок системы скидок без бг, добавил placeholder для input

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -68,7 +68,7 @@
                 <div class="card-pass d_flex f_col">
                     <h2 class="white_color">Проверьте Ваш пропуск</h2>
                     <form action="#" class="pass-form d_flex m_b_22">
-                        <input type="text" value="Введите номер авто">
+                        <input type="text" placeholder="Введите номер авто" value="">
                         <input type="submit" value="Заказать пропуск">
                     </form>
                     <p class="white_color">* Проверьте действие вашего пропуска в несколько кликов! Просто введите номерной знак транспортного средства и кликните по кнопке «Проверить».</p>
@@ -138,6 +138,28 @@
                         </div>
                     </div>
                 </div>
+            </div>
+        </section>
+
+        <section class="discount-system m_b_40">
+            <div class="__container">
+                    <h2>В нашей компании действует гибкая система скидок </h2>
+                    <p class="m_b_40">Оставьте заявку и наши менеджеры предложат Вам оптимальные условия сотрудничества</p>
+                    <div class="discount-info d_flex f_col f_gap_35">
+                        <div class="discount-choice__list d_flex f_gap_25">
+                            <a href="#" class="btn-common btn-active">От 5 автомобилей</a>
+                            <a href="#" class="btn-common">От 10 автомобилей</a>
+                            <a href="#" class="btn-common">От 15 автомобилей</a>
+                        </div>
+
+                        <div class="discount-personal">
+                            <form action="" class="discount-personal__form d_flex f_gap_25">
+                                <input type="text" placeholder="Введите номер авто" value="">
+                                <input type="tel" placeholder="+ 7 (___) ___ __ __" value="">
+                                <input type="submit" value="Отправить" class="btn">
+                            </form>
+                        </div>
+                    </div>
             </div>
         </section>
 </body>
