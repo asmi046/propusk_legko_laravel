7fac7c09b78df4bd14b4c863d70a62090771de64
asmi046 <asmi046@gmail.com>
2023-04-14 17:30:18 +0300

    Компоненты

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,39 +14,9 @@
     ])
 </head>
 <body>
-    <header class="p_y_40">
-        <div class="__container">
-        <div class="header d_flex jc_sb f_a_center">
-            <div class="header-logo d_flex">
-                <img src="/img/main-logo.svg" alt="Логотип пропуск легко">
-                <p>ПРОПУСК<br>ЛЕГКО</p>
-            </div>
-            <div class="header-nav">
-                <ul class="nav-menu d_flex f_gap_40">
-                    <li class="menu-list">
-                        <a href="#" class="list-items">О нас</a>
-                    </li>
-                    <li class="menu-list">
-                        <a href="#" class="list-items">Услуги</a>
-                    </li>
-                    <li class="menu-list">
-                        <a href="#" class="list-items">Выбрать массу</a>
-                    </li>
-                    <li class="menu-list">
-                        <a href="#" class="list-items">Выбрать зону</a>
-                    </li>
-                    <li class="menu-list">
-                        <a href="#" class="list-items">Контакты</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="header-call d_flex f_a_center f_gap_40">
-                <a href="tel:88003223344" class="call-number blue_color">8 800 322 33 44</a>
-                <a href="tel:88003223344" class="btn">Позвоните мне</a>
-            </div>
-        </div>
-        </div>
-    </header>
+
+    <x-header></x-header>
+
     <section class="main-block m_b_40">
             <div class="__container">
             <div class="main-page ">
