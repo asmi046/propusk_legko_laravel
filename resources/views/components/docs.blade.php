386b6e4f968d63571d95a4632ae13ee848ed5810
MinotourDmitriy <Kkunkka1@mail.ru>
2023-04-19 23:57:46 +0300

    img-block object-fit:contain

--- resources/views/components/docs.blade.php
+++ resources/views/components/docs.blade.php
@@ -3,7 +3,9 @@
         <h2>Список необходимых документов</h2>
         <div class="need-document d_flex f_gap_30">
             <div class="need-document__list border_block d_flex f_col">
+                <div class="need-document__list-wrapper">
                 <img src="/img/pts-img.png" alt="ПТС пример документа">
+                </div> 
                 <div class="need-document__list-info">
                 <p><span class="list-info__abbr">ПТС</span></p>
                 <p class="abbr-transcript">Паспорт технического средства</p>
@@ -12,7 +14,9 @@
             </div>
 
             <div class="need-document__list border_block d_flex f_col">
-                <img src="/img/sts-img.png" alt="СТС пример документа">
+            <div class="need-document__list-wrapper">
+            <img src="/img/sts-img.png" alt="СТС пример документа">
+            </div>
                 <div class="need-document__list-info">
                 <p><span class="list-info__abbr">СТС</span></p>
                 <p class="abbr-transcript">Свидетельство о Регистрации Технического Средства</p>
@@ -21,7 +25,9 @@
             </div>
 
             <div class="need-document__list border_block d_flex f_col">
-                <img src="/img/diagnostic-img.png" alt="Диагностическая карта пример документа">
+            <div class="need-document__list-wrapper">
+            <img src="/img/diagnostic-img.png" alt="Диагностическая карта пример документа">
+            </div>
                 <div class="need-document__list-info">
                 <p><span class="list-info__abbr">Диагностическая Карта</span></p>
                 <p class="abbr-transcript"></p>
@@ -30,7 +36,9 @@
             </div>
 
             <div class="need-document__list border_block d_flex f_col">
-                <img src="/img/driver_udost-img.png" alt="Водительское удостоверение пример документа">
+            <div class="need-document__list-wrapper">
+            <img src="/img/driver_udost-img.png" alt="Водительское удостоверение пример документа">
+            </div>           
                 <div class="need-document__list-info">
                 <p><span class="list-info__abbr">Водительское удостоверение</span></p>
                 <p class="abbr-transcript"></p>
