eda52160fd4b90f827ea935b5fbfef0791da0ab7
asmi046 <asmi046@gmail.com>
2023-04-22 17:32:20 +0300

    Og и закрываха меню

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,8 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
 let menu_btn = document.querySelectorAll(".toggle_menu")
+let menu_punkts = document.querySelectorAll(".puncts .menu-list a")
+
+for (let elem of menu_punkts)
+elem.addEventListener("click", function (e) {
+    for (let elem1 of menu_btn)
+        elem1.click();
+});
 
 for (let elem of menu_btn)
     elem.addEventListener("click", function (e) {
+
         elem.classList.toggle("active")
 
         let menu_elem = document.querySelector(".menu_wrapper")
--- resources/views/layouts/all.blade.php
+++ resources/views/layouts/all.blade.php
@@ -13,10 +13,9 @@
     <meta property="og:title" content="@yield('title')" />
     <meta property="og:description" content="@yield('description')" />
     <meta property="og:url" content="{{route('home')}}" />
-    <meta property="og:site_name" content="Подбор квартир в новостройках Курска" />
+    <meta property="og:site_name" content="Пропуск ЛЕГКО - попощь в получении пропуска на МКАД, ТТК, CK" />
     <meta property="og:image" content="{{asset('img/og_img.jpg')}}" />
     <meta property="og:image:type" content="image/jpeg" />
-    <meta name="twitter:card" content="summary_large_image" />
 
 
     <link rel="icon" type="image/png" href="{{asset('/img/favicons/icon256.png')}}" sizes="256x256">
