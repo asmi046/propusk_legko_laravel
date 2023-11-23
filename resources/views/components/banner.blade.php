168f0e563a4ca0c29b8f7b4b7d7615708ac41773
asmi046 <asmi046@gmail.com>
2023-11-23 13:10:11 +0300

    max_w_320

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -10,6 +10,6 @@
         <p><strong>Оплата после получения пропуска!</strong></p>
         <p><strong>Гарантия от аннуляции!</strong></p>
         <br>
-        <a href="#consultMsg" class="btn max_w_256">Получить консультацию</a>
+        <a href="#consultMsg" class="btn max_w_320">Получить консультацию</a>
     </div>
 </section>
